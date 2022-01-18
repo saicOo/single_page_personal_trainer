8bcac518390b0947b447376f7a447f4fc344b5e2
unknown <sayed.ahmed.elbadry@gmail.com>
2022-01-18 01:42:22 -0800

    add section why and offer

--- js/custom.js
+++ js/custom.js
@@ -182,7 +182,10 @@ document.addEventListener("DOMContentLoaded", function() {
     },runTime);
   });
 });
-
+//16. Overlay
+$(".snake").snakeify({
+  speed: 200
+});
 }(jQuery));
 
 // changs tabs
